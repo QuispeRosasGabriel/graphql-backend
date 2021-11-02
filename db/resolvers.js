7f6259ab85deb48220a1adac7611aa36cde25291
QuispeRosasGabriel <gabrielomarquisperosas@gmail.com>
2021-11-01 22:03:36 -0500

    new mutation

--- db/resolvers.js
+++ db/resolvers.js
@@ -2,6 +2,12 @@
 const resolvers = {
     Query: {
         getCourse: () => 'Smething'
+    },
+    Mutation: {
+        newUser: (_, {input}) => {
+            console.log(input)
+            return "Creando"
+        }
     }
 }
 
