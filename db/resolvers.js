2c9d569d5bd2ff5107d151988fb1bc3882ec802b
QuispeRosasGabriel <gabrielomarquisperosas@gmail.com>
2021-11-01 22:10:06 -0500

    working on mutation for new users

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,12 +1,25 @@
+const User = require('../models/User')
 
 const resolvers = {
     Query: {
         getCourse: () => 'Smething'
     },
     Mutation: {
-        newUser: (_, {input}) => {
-            console.log(input)
-            return "Creando"
+        newUser: async(_, {input}) => {
+            const {email, password} = input;
+            // Check if user exist 
+            const userAlreadyExist = await User.findOne({email});
+            if(userAlreadyExist) throw new Error("User already exist");
+           // Hash password
+           // Save on db
+
+           try {
+               const user = new User(input);
+               user.save();
+               return user;
+           } catch (error) {
+            throw new Error(`Unexpected Error ${error}`)
+           }
         }
     }
 }
